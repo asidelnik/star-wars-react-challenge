2892b1e88d4c42a4557417926b507165b7f257eb
Amos Sidelnik <asidelnik@gmail.com>
2022-02-04 12:39:09 +0200

    Get Pilots, homeWorlds, sort by population - not finished

--- src/components/vehicles-table/vehicles-table.js
+++ src/components/vehicles-table/vehicles-table.js
@@ -1,42 +1,89 @@
-const axios = require('axios');
+import { getDefaultNormalizer, logDOM } from "@testing-library/react";
+import { useEffect, useState } from "react";
 
-function getVehicles() {
+const axios = require("axios");
+
+function getWithUrlRoot(urlRoot) {
   // axios.get('https://swapi.py4e.com/api/planets/3/')
-  axios.get('https://swapi.py4e.com/api/vehicles/')
-  .then(function (response) {
-    let vehiclesWithPilots = response.data.results.filter(v => v.pilots.length > 0);
-    console.log(vehiclesWithPilots);
-    let pilots = vehiclesWithPilots.map(v => v.pilots).flat();
-    console.log(pilots);
-    return vehiclesWithPilots;
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
+  return axios.get("https://swapi.py4e.com/api/" + urlRoot);
+}
+
+function getWithFullUrl(urlFull) {
+  return axios.get(urlFull);
+}
+
+function getData() {
+  return getWithUrlRoot("vehicles")
+    .then(function (response) {
+      console.log("vehicles", response.data.results);
+      let vehiclesWithPilots = response.data.results.filter(
+        (v) => v.pilots.length > 0
+      );
+      console.log("vehiclesWithPilots", vehiclesWithPilots);
+
+      let pilotUrls = vehiclesWithPilots.map((v) => v.pilots).flat();
+      return pilotUrls;
+    })
+    .then(function (pilotsUrls) {
+      console.log(pilotsUrls)
+      let pilots = pilotsUrls.map((p) => getWithFullUrl(p));
+      return Promise.allSettled(pilots)
+    })
+    .then(function (pilots) {
+      console.log("pilots", pilots)
+      let homeWorlds = pilots.map((p) => getWithFullUrl(p.value.data.homeworld));
+      return Promise.allSettled(homeWorlds)
+    })
+    .then(function (homeWorlds) {
+      console.log("homeWorlds", homeWorlds)
+      let homeWorldsSortedDesc = 
+        homeWorlds.map((p) => p.value.data)
+        .map(({population, url}) => ({population, url}))
+        .sort((a, b) => b.population - a.population);
+      console.log("homeWorldsSortedDesc", homeWorldsSortedDesc);
+
+      return Promise.allSettled(homeWorlds)
+    })
+
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    });
 }
 
 const VehiclesTable = (props) => {
-  let res = undefined;
-  getVehicles();
+  const [res, setRes] = useState()
 
-      return (
-        <table id="vehicle-table">
-          <tbody>
-            <tr>
-              <td>Vehicle name with the largest diameter sum {res && res.data.climate}</td>
-            </tr>
-            <tr>
-              <td>Related home planets and their respective diameter {res &&res.data.diameter}</td>
-            </tr>
-            <tr>
-              <td>Related pilot names {res && res.data.name}</td>
-            </tr>
-          </tbody>
-        </table>
-      );
-}
+  useEffect(() => {
+    const fn = async () => {
+      const data = await getData();
+      console.log({data})
+
+    }
+    fn()
+  }, [])
+  // let res = undefined;
+  // res = getData();
+
+  return (
+    <table id="vehicle-table">
+      <tbody>
+        <tr>
+          <td>
+            Vehicle name with the largest diameter sum {res && res.data.climate}
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Related home planets and their respective diameter{" "}
+            {res && res.data.diameter}
+          </td>
+        </tr>
+        <tr>
+          <td>Related pilot names {res && res.data.name}</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
 export default VehiclesTable;
